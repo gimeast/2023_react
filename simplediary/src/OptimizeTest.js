31ccb2dbe1e71dcbfa0f2bba92ea5a6248b4d6f2
gimeast94 <dudu.gimeast@gmail.com>
2023-07-27 13:15:47 +0900

    최적화 2 - React.memo(2)

--- simplediary/src/OptimizeTest.js
+++ simplediary/src/OptimizeTest.js
@@ -1,38 +1,36 @@
 import React, { useState, useEffect } from "react";
 
-const TextView = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`Update text:${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const CountView = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`Update count:${count}`);
+    console.log(`CounterA update :: ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = React.memo(({ obj }) => {
+  useEffect(() => {
+    console.log(`CounterB update :: ${obj.count}`); //객체의 경우는 같은 값이여도 얕은 비교를 하기때문에 리렌더링을 해버린다.
+  });
+  return <div>{obj.count}</div>;
+});
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
       </div>
       <div>
-        <h2>text</h2>
-        <TextView text={text} />
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+        <h2>Counter B</h2>
+        <CounterB obj={obj} />
+        <button onClick={() => setObj({ count: obj.count })}>B button</button>
       </div>
     </div>
   );
